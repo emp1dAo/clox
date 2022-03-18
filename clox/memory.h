2cf798e5cd9b444c9c992cdec096d8c2c8452693
shaochenheng <1010716589@qq.com>
2022-03-18 10:39:32 +0000

    Add memory manager.

--- clox/memory.h
+++ clox/memory.h
@@ -12,6 +12,9 @@
   (type*)reallocate(pointer, sizeof(type) * (oldCount), \
 		    sizeof(type) * (newCount))
 
+#define FREE_ARRAY(type, pointer, oldCount) \
+  reallocate(pointer, sizeof(type) * (oldCount), 0)
+
 // Allocating memory, freeing memory and changing the size of an existing allocation;
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
