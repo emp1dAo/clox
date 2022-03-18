629d4f7a9c2fb510acfe1b003100676b27b73c51
shaochenheng <1010716589@qq.com>
2022-03-18 09:10:24 +0000

    Try to add memory manager.

---
+++ clox/memory.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+#include "memory.h"
+
+// Allocating memory, freeing memory and changing the size of an existing allocation;
+void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+  if (newSize == 0) {
+    free(pointer);
+    return NULL;
+  }
+
+  void* result = realloc(pointer, newSize);
+  return result;
+} 
