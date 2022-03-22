3141ba8c63ef4283713aacc51fbc0194930d70e1
shaochenheng <1010716589@qq.com>
2022-03-22 22:47:46 +0000

    Try to add Vitual machine.

---
+++ clox/vm.h
@@ -0,0 +1,27 @@
+#ifndef clox_vm_h
+#define clox_vm_h
+
+#include "chunk.h"
+
+typedef struct {
+  Chunk* chunk;
+  
+  // IP means instruction pointer.
+  // x86, x64, and the CLR call it “IP”. 68k, PowerPC, ARM, p-code
+  // and the JVM call it “PC”, for program counter.
+  uint8_t* ip; 
+} VM;
+
+typedef enum {
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM();
+
+void freeVM();
+
+InterpretResult interpret(chunk* chunk);
+
+#endif
