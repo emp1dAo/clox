cfcbe7ee381cf7560dc8050f1b19c425761b7b98
shaochenheng <1010716589@qq.com>
2022-03-26 23:36:02 +0000

    Try to add Vitual machine.

--- clox/vm.c
+++ clox/vm.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "common.h"
+#include "debug.h"
 #include "vm.h"
 
 VM vm;
@@ -13,10 +15,22 @@ void freeVM() {
 
 static InterpretResult run() {
   #define READ_BYTE() (*vm.ip ++)
-
+  #define READ_CONSTANT() (vm.chunk -> constants.values[READ_BYTE()])
+  
   for(;;) {
+    #ifdef DEBUG_TRACE_EXECUTION
+    disassembleInstruction(vm.chunk,
+			   (int)(vm.ip - vm.chunk->code));
+    #endif
+    
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
+    case OP_CONSTANT: {
+      Value constant = READ_CONSTANT();
+      printValue(constant);
+      printf("\n");
+      break;
+    }
     case OP_RETURN: {
       return INTERPRET_OK;
     }
@@ -24,6 +38,7 @@ static InterpretResult run() {
     }
   }
   #undef READ_BYTE
+  #undef READ_CONSTANT
 }
 
 InterpretResult interpret(Chunk* chunk) {
