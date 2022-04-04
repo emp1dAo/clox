9aa1921c7e0efb74678c5d1c061893942238d62f
shaochenheng <1010716589@qq.com>
2022-04-04 23:38:45 +0000

    Try to add scanner and compiler.

--- clox/main.c
+++ clox/main.c
@@ -23,13 +23,22 @@ static void repl() {
 
 static char* readFile(const char* path) {
   FILE* file = fopen(path, "rb");
+  if (file == NULL) {
+    fprintf(stderr, "Could not open file \"%s\".\n".path);
+    exit(74);
+  }
 
   fseek(file, 0L, SEEK_END);
   size_t fileSize = ftell(file);
   rewind(file);
 
   char* buffer = (char*)malloc(fileSize + 1);
-  size_t = bytesRead = fread(buffer, sizeof(char), fileSize, file);
+  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+  if (bytesRead < fileSize) {
+    fprintf(stedrr, "Could not read file \"%s\".\n", path);
+    exit(74);
+  }
+  
   buffer[bytesRead] = '\0';
 
   fclose(file);
