0bf349b978bca158a58a2b0d6ad31c94ceda4dac
shaochenheng <1010716589@qq.com>
2022-04-12 08:42:50 +0000

    Temp update.

--- clox/compiler.c
+++ clox/compiler.c
@@ -4,6 +4,48 @@
 #include "compiler.h"
 #include "scanner.h"
 
+typedef struct {
+  Token current;
+  Token previous;
+  bool hadError;
+} Parser;
+
+Parser parser;
+
+static void errorAt(Token* token, const char* message) {
+  fprintf(stderr, "[line %d] Error", token -> line);
+
+  if (token -> type == TOKEN_EOF) {
+    fprintf(stderr, " at end");
+  } else if (token -> type == TOKEN_ERROR) {
+    // nothing
+  } else {
+    fprintf(stderr, " at '%.*s'", token -> length, token -> start);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.hadError = true;
+}
+
+static void error(const char* message) {
+  errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char* message) {
+  errorAt(&parser.current, message);
+}
+
+static void advance() {
+  paser.previous = parser.current;
+
+  for (;;) {
+    parser.current = scanToken();
+    if (parser.current.type != TOKEN_ERROR) break;
+
+    errorAtCurrent(parser.current.start);
+  }
+}
+
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
   advance();
