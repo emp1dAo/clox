e987143d46950619b4cb76b48e232d8f0031c8e8
shaochenheng <1010716589@qq.com>
2023-04-16 19:54:31 +0000

    Add method to implement global variable.

--- clox/value.h
+++ clox/value.h
@@ -3,13 +3,11 @@
 
 #include "common.h"
 
-typedef double Value;
-
-typedef struct {
-  int capacity;
-  int count;
-  Value* values;
-} ValueArray;
+typedef enum {
+  VAL_BOOL,
+  VAL_NIL,
+  VAL_NUMBER,
+} ValueType;
 
 typedef struct {
   ValueType type;
@@ -18,6 +16,24 @@ typedef struct {
     double number;
   } as; 
 } Value;
+
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
+
+typedef struct {
+  int capacity;
+  int count;
+  Value* values;
+} ValueArray;
+
+bool valuesEqual(Value a, Value b);
 
 void initValueArray(ValueArray* array);
 
