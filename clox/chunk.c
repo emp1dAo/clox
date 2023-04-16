3bb50cdc7596bc8ee0b4889098863f89360b7a4e
shaochenheng <1010716589@qq.com>
2023-04-16 19:49:32 +0000

    Add method to implement logic caculate.

--- clox/chunk.c
+++ clox/chunk.c
@@ -12,15 +12,15 @@ void initChunk(Chunk* chunk) {
   initValueArray(&chunk -> constants);
 }
 
-// Initialize a new chunk;
+// Append a byte to the end of the chunk;
 void writeChunk(Chunk* chunk, uint8_t byte, int line) {
   if (chunk -> capacity < chunk -> count + 1) {
     int oldCapacity = chunk -> capacity;
     chunk -> capacity = GROW_CAPACITY(oldCapacity);
     chunk -> code = GROW_ARRAY(uint8_t, chunk -> code,
-			       oldCapacity, chunk -> capacity);
+                               oldCapacity, chunk -> capacity);
     chunk -> lines = GROW_ARRAY(int, chunk -> lines,
-			       oldCapacity, chunk -> capacity);
+                                oldCapacity, chunk -> capacity);
   }
 
   chunk -> code[chunk -> count] = byte;
--- clox/chunk.h
+++ clox/chunk.h
@@ -4,9 +4,17 @@
 #include "common.h"
 #include "value.h"
 
+// Define opcode;
 typedef enum {
   OP_RETURN,
   OP_CONSTANT,
+  OP_NIL,
+  OP_TRUE,
+  OP_FALSE,
+  OP_EQUAL,
+  OP_GREATER,
+  OP_LESS,
+  OP_NOT,
   OP_NEGATE,
   OP_ADD,
   OP_SUBTRACT,
@@ -14,9 +22,17 @@ typedef enum {
   OP_DIVIDE,
 } OpCode;
 
+/*
+  Byte is a series of instructions,
+  which will include other data,
+  use a struct to save these;
+*/
 typedef struct {
+  // Have allocated;
   int count;
+  // Allocated entries are actually in use;
   int capacity;
+  // A dynamic array of instructions;
   uint8_t* code;
   int* lines;
   ValueArray constants;
