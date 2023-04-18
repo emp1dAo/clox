7b2f22039379de148a91bb8d157b85c0cc04223b
shaochenheng <1010716589@qq.com>
2023-04-18 22:29:34 +0000

    Add hash table.

--- clox/value.c
+++ clox/value.c
@@ -45,6 +45,8 @@ bool valuesEqual(Value a, Value b)  {
   case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
   case VAL_NIL:    return true;
   case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+  case VAL_OBJ:    return AS_OBJ(a) == AS_OBJ(b);
+  /*
   case VAL_OBJ: {
     ObjString* aString = AS_STRING(a);
     ObjString* bString = AS_STRING(b);
@@ -52,6 +54,7 @@ bool valuesEqual(Value a, Value b)  {
       memcmp(aString -> chars, bString -> chars,
              aString -> length) == 0;
   }
+  */
   default:         return false;
   }
 }
