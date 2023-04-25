51bdb7abb78564685fe89c5bd226e3b7d19f19a9
scheng <scheng52123@gmail.com>
2023-04-25 22:55:45 +0800

    Add closure.

--- clox/vm.h
+++ clox/vm.h
@@ -9,6 +9,7 @@
 #define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
 
 typedef struct {
+  ObjClosure* closure;
   ObjFunction* function;
   uint8_t* ip;
   Value* slots;
@@ -31,6 +32,7 @@ typedef struct {
   Value* stackTop;
   Table globals;
   Table strings;
+  ObjUpvalue* openUpvalues;
   Obj* objects;
 } VM;
 
